b9c44c289784319b992070614ee900c0ef7f758e
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2016-12-15 10:52:45 +0100

    Update letsencrypt.pp

--- manifests/letsencrypt.pp
+++ manifests/letsencrypt.pp
@@ -3,7 +3,7 @@
 class role_apache::letsencrypt {
 
   class { ::certbot:
-    manage_dependencies => false,
+    manage_dependencies => false, # otherwise duplicate declaration package git in ubuntu 14.04
     config              => $role_apache::letsencrypt_config,
   }
 
